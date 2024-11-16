aa6c50995603c96d01cfef5b8160900fac4afbc3
Derrek Gass <dardcore@gmail.com>
2024-11-15 17:53:58 -0800

    adding in some init data for scales

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [scales, setScales] = useState<string[]>([])
+  const [selectedScale, setSelectedScale] = useState<string>('')
 
+  useEffect(() => {
+    fetch('/theory.json')
+      .then(response => response.json())
+      .then(data => {
+        setScales(data.scales)
+      })
+      .catch(error => console.error('Error loading scales:', error))
+  }, [])
+
+  const handleScaleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedScale(event.target.value)
+  }
+
+  console.log(scales['major'])
   return (
     <>
       <div>
