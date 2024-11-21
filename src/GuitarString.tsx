2a3708af1323c3e9ff8c0a5783e588af08684375
Derrek Gass <dardcore@gmail.com>
2024-11-20 21:02:58 -0800

    adding colored circles

--- src/GuitarString.tsx
+++ src/GuitarString.tsx
@@ -10,15 +10,17 @@ function GuitarString(props: { notes: string[], keys: string[], string: string, 
     let spans = [];
     for (let i = 0; i < frets; i++) {
       const note = keys[(stringIndex + i) % keys.length];
+      console.log(note);
       const stringWidth = 48 - i;
       if (notes.includes(note)) {
+        const backgroundColor = note === notes[0] ? "#FF0000" : note === notes[4] ? "green" : note === notes[2] ? "yellow" : note === notes[6] ? "purple" : "teal";
         spans.push(
-            <span style={{backgroundColor: "green", borderRadius: "25px", padding: "5px", display: "flex"}}>
-              <span style={{marginRight: `${stringWidth}px`, textAlign: "center"}}>{note}</span>
-            </span>
+          <div style={{ textAlign: "center", width: `${stringWidth}px`, display: "flex", justifyContent: "center", borderRight: "5px solid silver"}}>
+            <span style={{ backgroundColor: `${backgroundColor}`, width: "3ch", height: "3ch", borderRadius: "50%"}}>{note}</span>
+            </div>
             );
       } else {
-        spans.push(<span style={{marginRight: `${stringWidth}px`, textAlign: "center", padding: "5px", display: "flex"}}>{note}</span>);
+        spans.push(<div style={{width: `${stringWidth}px`, textAlign: "center", borderRight: "5px solid silver"}}>{note}</div>);
       }
     }
     return spans;
@@ -26,7 +28,7 @@ function GuitarString(props: { notes: string[], keys: string[], string: string, 
 
     return (
       <>
-      <div style={{justifyContent: "center", display: "flex"}}>
+      <div style={{justifyContent: "center", display: "flex", textAlign: "center", backgroundColor: "#f5ece4"}}>
         {buildScaleForString(string, frets)}
       </div>
       </>
